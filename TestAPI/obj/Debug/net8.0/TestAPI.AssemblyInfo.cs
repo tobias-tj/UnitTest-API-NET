eeb68fa0f27e7e25b03aee7c2aa7129b48f71986
tobias-tj <https.tobias.jara.404@gmail.com>
2024-08-10 16:25:20 -0400

    Create gitignore

--- TestAPI/obj/Debug/net8.0/TestAPI.AssemblyInfo.cs
+++ TestAPI/obj/Debug/net8.0/TestAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a48bcc6770b16fe6c214e94b9002e3436395694")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
